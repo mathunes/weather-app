a7a1bc893fa75b41b26fee25bd52f19b11ef40aa
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-13 10:32:48 -0300

    Configuracoes do store

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 export default class SearchInput extends Component {
     constructor() {
         super();
-        
+
         this.state = {
             search: ''
         }
 
         this.handleChangeInput = this.handleChangeInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChangeInput(e) {
@@ -17,10 +18,15 @@ export default class SearchInput extends Component {
         })
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.search)
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" placeholder="Cidade, estado, pais" onChange={this.handleChangeInput}/>
                 </form>
             </div>
