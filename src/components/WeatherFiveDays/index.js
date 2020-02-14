ea1b45efc4c1b026b30488705a91ad7f49368f6f
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-13 21:47:18 -0300

    Troca da exibicao do dia dentre a previsao dos cinco dias

--- src/components/WeatherFiveDays/index.js
+++ src/components/WeatherFiveDays/index.js
@@ -7,29 +7,71 @@ class WeatherFiveDays extends Component {
 
     state = {
         date: new Date().getDate(),
-        month: new Date().getMonth() + 1
-    }
-
-    componentDidUpdate() {
-        console.log(this.props)
+        month: new Date().getMonth() + 1,
+        weatherDayView: (new Date().getDate()).toString()
     }
 
     render() {
+
+        let container;
+
+        if (this.props.fiveDaysWeather.found) {
+            if (!this.props.fiveDaysWeather.loading) {
+                container =
+                <div>
+                    {this.props.fiveDaysWeather.data.list.map((item, i) => {
+                        return (
+                            (item.dt_txt.substr(8, 2) === this.state.weatherDayView) ? 
+                                <div key={i}>
+                                    <span>{item.dt_txt.substr(11, 5)}</span><br />
+                                    <span>{(item.main.temp - 273.15).toFixed(2)}°C</span>    
+                                </div>
+                            : ''
+                        )
+                    })}
+                    
+                </div>
+            }
+        }
         
         return (
             <div>
                 <table>
                     <thead>
                         <tr>
-                            <th>Today</th>
-                            <th>Tomorrow</th>
-                            <th>{`${this.state.date + 2}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}</th>
-                            <th>{`${this.state.date + 3}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}</th>
-                            <th>{`${this.state.date + 4}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}</th>
+                            <th 
+                                onClick={() => this.setState(
+                                    {weatherDayView: (new Date().getDate()).toString()}
+                                )}>
+                                Today
+                            </th>
+                            <th onClick={() => this.setState(
+                                    {weatherDayView: (new Date().getDate() + 1).toString()}
+                                )}>
+                                Tomorrow
+                            </th>
+                            <th 
+                                onClick={() => this.setState(
+                                    {weatherDayView: (new Date().getDate() + 2).toString()}
+                                )}>
+                                {`${this.state.date + 2}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                            </th>
+                            <th 
+                                onClick={() => this.setState(
+                                    {weatherDayView: (new Date().getDate() + 3).toString()}
+                                )}>
+                                {`${this.state.date + 3}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                            </th>
+                            <th 
+                                onClick={() => this.setState(
+                                    {weatherDayView: (new Date().getDate() + 4).toString()}
+                                )}>
+                                {`${this.state.date + 4}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                            </th>
                         </tr>
                     </thead>
                 </table>
-
+                {container}
             </div>
         )
     }
