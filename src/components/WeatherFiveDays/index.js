551378de1badf3874b082f4fe377ab68a6ce7e73
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-14 11:02:08 -0300

    Correcao na estrutura semantica do componente 'WeatherFiveDays'

--- src/components/WeatherFiveDays/index.js
+++ src/components/WeatherFiveDays/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as actionSearch from '../../actions'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { ContainerWeatherFiveDays } from './styles';
 
 class WeatherFiveDays extends Component {
 
@@ -35,44 +36,42 @@ class WeatherFiveDays extends Component {
         }
         
         return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th 
-                                onClick={() => this.setState(
-                                    {weatherDayView: (new Date().getDate()).toString()}
-                                )}>
-                                Hoje
-                            </th>
-                            <th onClick={() => this.setState(
-                                    {weatherDayView: (new Date().getDate() + 1).toString()}
-                                )}>
-                                Amanhã
-                            </th>
-                            <th 
-                                onClick={() => this.setState(
-                                    {weatherDayView: (new Date().getDate() + 2).toString()}
-                                )}>
-                                {`${this.state.date + 2}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
-                            </th>
-                            <th 
-                                onClick={() => this.setState(
-                                    {weatherDayView: (new Date().getDate() + 3).toString()}
-                                )}>
-                                {`${this.state.date + 3}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
-                            </th>
-                            <th 
-                                onClick={() => this.setState(
-                                    {weatherDayView: (new Date().getDate() + 4).toString()}
-                                )}>
-                                {`${this.state.date + 4}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
-                            </th>
-                        </tr>
-                    </thead>
-                </table>
+            <ContainerWeatherFiveDays>
+                <nav>
+                    <ul>
+                        <li 
+                            onClick={() => this.setState(
+                                {weatherDayView: (new Date().getDate()).toString()}
+                            )}>
+                            Hoje
+                        </li>
+                        <li onClick={() => this.setState(
+                                {weatherDayView: (new Date().getDate() + 1).toString()}
+                            )}>
+                            Amanhã
+                        </li>
+                        <li 
+                            onClick={() => this.setState(
+                                {weatherDayView: (new Date().getDate() + 2).toString()}
+                            )}>
+                            {`${this.state.date + 2}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                        </li>
+                        <li 
+                            onClick={() => this.setState(
+                                {weatherDayView: (new Date().getDate() + 3).toString()}
+                            )}>
+                            {`${this.state.date + 3}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                        </li>
+                        <li 
+                            onClick={() => this.setState(
+                                {weatherDayView: (new Date().getDate() + 4).toString()}
+                            )}>
+                            {`${this.state.date + 4}/${(this.state.month < 10) ? '0' : ''}${this.state.month}`}
+                        </li>
+                    </ul>
+                </nav>
                 {container}
-            </div>
+            </ContainerWeatherFiveDays>
         )
     }
 }
---
+++ src/components/WeatherFiveDays/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ContainerWeatherFiveDays = styled.div`
+    margin: 0 auto;
+    width: 400px;
+    max-width: 400px;
+`
