47a39f85c42060487eaa1377037d00ad2fd22530
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-17 16:26:08 -0300

    Updates

---
+++ precache-manifest.b7e3a1553183b4ea3b98ae9380de37bb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27973217c6e62987470547e87c93b07",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7af40fa1e211c500f5a4",
+    "url": "/weather-app/static/css/main.63843ce0.chunk.css"
+  },
+  {
+    "revision": "ddfc6cc8357a776929dd",
+    "url": "/weather-app/static/js/2.7be4d4f8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/weather-app/static/js/2.7be4d4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af40fa1e211c500f5a4",
+    "url": "/weather-app/static/js/main.fe2d7e71.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  },
+  {
+    "revision": "f08f813dc7b9066efb48574644473664",
+    "url": "/weather-app/static/media/cold-1.f08f813d.png"
+  },
+  {
+    "revision": "cb6130ae2e9425bc4e247b13929e0ae8",
+    "url": "/weather-app/static/media/cold-2.cb6130ae.png"
+  },
+  {
+    "revision": "5b6083fdded403ce7fbfa73e0622149a",
+    "url": "/weather-app/static/media/dark-1.5b6083fd.png"
+  },
+  {
+    "revision": "b14bedb5ae2d9d9b26e2add510eaad7b",
+    "url": "/weather-app/static/media/dark-2.b14bedb5.png"
+  },
+  {
+    "revision": "8c746a4aa1f2e61927664bca13545d9c",
+    "url": "/weather-app/static/media/dust-1.8c746a4a.png"
+  },
+  {
+    "revision": "95ebddbcc47faf2761397d7870372fc1",
+    "url": "/weather-app/static/media/dust-2.95ebddbc.png"
+  },
+  {
+    "revision": "3f05c3dc13ae94ef09a153fac05e6b5a",
+    "url": "/weather-app/static/media/loading.3f05c3dc.gif"
+  },
+  {
+    "revision": "30058d62b3c18df1b177003b4388568b",
+    "url": "/weather-app/static/media/sunny-1.30058d62.png"
+  },
+  {
+    "revision": "dfd1b93fe3c8be6b36f883ecc0df4afc",
+    "url": "/weather-app/static/media/sunny-2.dfd1b93f.png"
+  }
+]);
