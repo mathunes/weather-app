1d752c4c8b9d8ffe38810c421df7e4ed1fba0f02
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-17 16:45:42 -0300

    Correcao na altura do site

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -5,7 +5,7 @@ export const ContainerComponents = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-evenly;
-    height: 100vh;    
+    min-height: 100vh;    
     background: url( ${props => props.weather} ) no-repeat center center fixed; 
     background-size: cover;
 
